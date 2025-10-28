06d9975a7ce791c008c919f321889f1e7da5a83d
Luis-Park <Plurilock@MacBook-Pro-198.local>
2025-10-28 15:51:02 -0400

    fixed input type

--- lambda_functions/call_stepfn.py
+++ lambda_functions/call_stepfn.py
@@ -9,10 +9,11 @@ STATE_MACHINE_ARN = os.environ.get("STEP_FUNCTION_ARN")
 
 
 def lambda_handler(event, context):
-    
+    step_input = json.dumps({"foo1": "bar1!"})
+
     response = stepfn_client.start_execution(
         stateMachineArn=STATE_MACHINE_ARN,
-        input={"foo1": "bar1!"}
+        input=step_input
     )
     print("Started execution:", response["executionArn"])
 
